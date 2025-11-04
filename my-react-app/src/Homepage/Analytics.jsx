c9c372a022330612bd5b836cfde4d1f284e27d50
Kirkspoker <pradokirby15@gmail.com>
2025-11-04 23:50:50 +0800

    analytics

--- my-react-app/src/Homepage/Analytics.jsx
+++ my-react-app/src/Homepage/Analytics.jsx
@@ -1,17 +1,41 @@
+import { useState } from "react";
 import Sidebar from "./Sidebar.jsx";
 import Style from "./index.module.css";
 import { Chart as ChartJS } from "chart.js/auto";
 import { Bar, Doughnut, Line } from "react-chartjs-2";
 
-function Analytics() {
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRange, Calendar } from 'react-date-range';
+import { addDays, format } from "date-fns";
 
-    const data = [
-        { name: "Jan", users: 30 },
-        { name: "Feb", users: 45 },
-        { name: "Mar", users: 80 },
-        { name: "Apr", users: 60 },
-        { name: "May", users: 120 },
-    ];
+function Analytics() {
+    const [datePick, isDatePick] = useState(false);
+    const [rangeDate, isRangeDate] = useState(false);
+    const [dateChanged, isDateChanged] = useState(false);
+
+    const [date, setDate] = useState({
+        startDate: new Date(),
+        endDate: new Date(),
+        key: 'selection'
+    });
+    
+    const getDateRangeLabels = (start, end) => {
+        const labels = [];
+        let curr = start;
+
+        while (curr <= end) {
+            labels.push(format(curr, "MMM-dd-yyyy"));
+            curr = addDays(curr, 1);
+        }
+
+        return labels;
+    };
+
+    const handleChange = (ranges) => {
+        setDate(ranges.selection);
+        isDateChanged(true); /* To display the selected date */
+    }
 
     return(
         <>
@@ -40,18 +64,150 @@ function Analytics() {
                         </div>
                     </div>
                     <div className={Style.charts}>
+                        {/* ATTENDANCE CHARTS */}
                         <div className={Style.attendance_chart}>
-                            <Bar 
-                                data={{
-                                    labels: ["A", "B", "C"],
-                                    datasets: [
-                                        {
-                                            label: "Revenue",
-                                            data: [200, 300, 400],
-                                        },
-                                    ]
-                                }}
-                            />
+                            <h3>Attendance Charts</h3>
+                            <div className={Style.charts_container}>
+                                <div className={Style.bar_graph}>
+                                     <Bar 
+                                        data={{
+                                            labels: getDateRangeLabels(date.startDate, date.endDate) /* Display the selected dates as labels */,
+                                            datasets: [
+                                                {
+                                                    label: "Present",
+                                                    data: [400, 300, 800],
+                                                },
+                                                {
+                                                    label: "Absent",
+                                                    data: [100, 300, 100],
+                                                },
+                                                {
+                                                    label: "Late",
+                                                    data: [10, 500, 400],
+                                                },
+                                                {
+                                                    label: "Undertime",
+                                                    data: [50, 300, 100],
+                                                },
+                                                {
+                                                    label: "Leave",
+                                                    data: [100, 700, 10],
+                                                }
+                                            ],
+                                        }}
+                                    />
+                                    <div className={Style.date_picker}>
+                                        <button className={Style.date_picker_btn} onClick={() => isDatePick(!datePick)}>
+                                            {dateChanged ? `` : 'Select Date' /* Display the selected date if date is selected */} 
+                                            <i class='bxr  bx-calendar-detail' ></i> 
+                                        </button>
+                                        <div className={`${datePick ? Style.select_date : ''} ${Style.date_range}`}> 
+                                            <Calendar /* Date picker for charts */
+                                                ranges={[date]}
+                                                onChange={handleChange}
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={Style.doughnut_graph}>
+                                    <Doughnut 
+                                        data={{
+                                            labels: ["Emergency Leave", "Sick Leave", "Vacation Leave"],    
+                                            datasets: [
+                                                {
+                                                    data: [500, 300, 50]
+                                                }
+                                            ]
+                                        }}
+                                        options={{
+                                            plugins: {
+                                                title: {
+                                                    display: true,
+                                                    text: `${format(date.startDate, 'MMM dd, yyyy')} - ${format(date.endDate, 'MMM dd, yyyy')}`,
+                                                    position: "bottom"
+                                                },
+                                            },
+                                        }}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        {/* PROJECTED CHARTS */}
+                        <div className={Style.projected_charts}>
+                            <h3>Projected Charts</h3>
+                            <div className={Style.charts_container}>
+                                <div className={Style.projected_salary}>
+                                    <Bar 
+                                        data={{
+                                            labels: getDateRangeLabels(date.startDate, date.endDate),
+                                            datasets: [
+                                                {
+                                                    data: [100000, 200000, 300000]
+                                                }
+                                            ]
+                                        }}
+                                        options={{
+                                            plugins: {
+                                                title: {
+                                                    display: true,
+                                                    text: `Projected Salary Payout, ${format(date.startDate, 'MMM dd, yyyy')} - ${format(date.endDate, 'MMM dd, yyyy')}`,
+                                                    position: "top",
+                                                    font: {
+                                                        size: 18,
+                                                        weight: 300
+                                                    }
+                                                },
+                                                legend: {
+                                                    display: false,
+                                                }
+                                            }
+                                        }}
+                                    />
+                                    <div className={Style.range_date}>
+                                        <button className={Style.bar_graph_date_btn} onClick={() => isRangeDate(!rangeDate)}>
+                                            {dateChanged ? `${format(date.startDate, 'MM,dd,yyyy')} - ${format(date.endDate, 'MM,dd,yyyy')}` : 'Select Date' /* Display the selected date if date is selected */} 
+                                            <i class='bxr  bx-calendar-detail' ></i> 
+                                        </button>
+                                        <div className={`${rangeDate ? Style.select_date : ''} ${Style.date_range}`}> 
+                                            <DateRange /* Date picker for charts */
+                                                ranges={[date]}
+                                                onChange={handleChange}
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={Style.projected_attendance}>
+                                    <Bar 
+                                        data={{
+                                            labels: getDateRangeLabels(date.startDate, date.endDate),
+                                            datasets: [
+                                                {
+                                                    label: "Projected Attendance (days)",
+                                                    data: [100, 200, 300],
+                                                },
+                                                {
+                                                    
+                                                    label: "Projected Absences (days)",
+                                                    data: [500, 110, 230]
+                                                }
+                                            ]
+                                        }}
+                                        options={{
+                                            plugins: {
+                                                title: {
+                                                    display: true,
+                                                    text: `Projected Attendance (days) and Projected Absences (days)`,
+                                                    position: "top",
+                                                    font: {
+                                                        size: 18,
+                                                        weight: 300
+                                                    }
+                                                }
+                                            }
+                                        }}
+                                    />
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
